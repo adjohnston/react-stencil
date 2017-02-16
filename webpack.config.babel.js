8fbe25148f8fbc4513cac769e2969f29a281ed0a
Adam Johnston <adam@getkahoot.com>
2017-02-16 15:02:07 +0000

    react dev server + config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,4 +1,5 @@
 import { resolve } from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 
 export default {
   entry: resolve(__dirname, 'src/index.js'),
@@ -8,11 +9,25 @@ export default {
     path: resolve(__dirname, 'dist')
   },
 
+  devtool: 'eval',
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, 'src/index.html')
+    })
+  ],
+
   module: {
     rules: [{
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel-loader'
     }]
+  },
+
+  resolve: {
+    alias: {
+      components: resolve(__dirname, 'src/components')
+    }
   }
 }
