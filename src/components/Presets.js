933cff3f47aa27215b16d08fa8c6e84c581786c9
Adam Johnston <adam.johnston3679@gmail.com>
2017-02-19 22:04:36 +0000

    empty object as default

--- src/components/Presets.js
+++ src/components/Presets.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react'
 
-const Presets = ({presets, setPreset}) => (
+const Presets = ({presets = {}, setPreset}) => (
   <div>
     {Object.keys(presets).map(preset => (
       <button
