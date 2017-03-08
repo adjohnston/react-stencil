83a8e316a8aa2c063b18930ae2aedc7751a6ffe5
Adam Johnston <adam@getkahoot.com>
2017-03-08 16:41:00 +0000

    beginning of reading components for their prop types

--- src/build.js
+++ src/build.js
@@ -1,3 +1,16 @@
 #! /usr/bin/env node
 
-console.log('I live!')
+var fs = require('fs')
+var glob = require('globby')
+var argv = require('minimist')(process.argv.slice(2), {
+  string: 'components',
+  alias: {
+    c: 'components'
+  }
+})
+
+glob(`${argv.c}/**/*.?(js|jsx)`)
+  .then((paths) => {
+    debugger
+    paths
+  })
