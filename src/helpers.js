673b6c684f9b64944bdde43fd7424f4f06edf186
Adam Johnston <adam.johnston3679@gmail.com>
2017-03-18 10:12:06 +0000

    `getComponentName` function

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,22 @@
+const R = require('ramda')
 const path = require('path')
 
+//    splitOnHyphen : string -> array
+const splitOnHyphen = (str) => {
+  return R.split('-', str)
+}
+
+//    upperCaseWords : array -> array
+const upperCaseWords = (words) => {
+  return R.map(word => word.toUpperCase(), words)
+}
+
+//    joinChars : array -> string
+const joinChars = (chars) => {
+  return R.join('', chars)
+}
+
+//    getPathName : string -> string
 const getPathName = (p) => {
   const bN = path.basename(p).split('.')
   if (bN.length > 1)
@@ -7,6 +24,19 @@ const getPathName = (p) => {
   throw new Error(`path ${p} must include component file`)
 }
 
+//    getComponentName : string -> string
+const getComponentName = (componentPathName) => (
+  R.pipe(
+    splitOnHyphen,
+    upperCaseWords,
+    joinChars
+  )(componentPathName)
+)
+
 module.exports = {
-  getPathName
+  splitOnHyphen,
+  upperCaseWords,
+  joinChars,
+  getPathName,
+  getComponentName
 }
