2f0affd0cd92b43d65013251de11ed33e3de6f6f
Adam Johnston <adam.johnston3679@gmail.com>
2017-03-19 08:33:36 +0000

    more className tests

--- tests/component-helpers.test.js
+++ tests/component-helpers.test.js
@@ -3,7 +3,15 @@ import {
 } from '../src/helpers'
 
 describe('#className', () => {
-  test(`given an 'title' as an elemen param expect 'rctnry__title'`, () => {
+  test(`given 'title' as an element param expect 'rctnry__title'`, () => {
     expect(className('title')).toBe('rctnry__title')
+  })
+
+  test(`given 'large' as a modifier param expect 'rctnry--large'`, () => {
+    expect(className(null, 'large')).toBe('rctnry--large')
+  })
+
+  test(`given 'title' as an element param & 'large' as a modifier param expect 'rctnry__title--large'`, () => {
+    expect(className('title', 'large')).toBe('rctnry__title--large')
   })
 })
