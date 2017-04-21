12f92bee05d3fec4c71b846181962e7f04ffced5
Adam Johnston <adam@getkahoot.com>
2017-04-21 11:24:46 +0100

    uncomment

--- src/cli.js
+++ src/cli.js
@@ -38,73 +38,73 @@ inquirer.prompt([
     message: 'Do you want to automagically generate component mapping?',
     default: true
   }
-]).then(answers => console.log(answers))
+]).then(answers => {
+  glob(Array.isArray(argv.c) ? argv.c.map(getComponentPaths) : getComponentPaths(argv.c))
+    .then((componentPaths) => {
+      fs.ensureFile(path.resolve(argv.d, 'global-definitions.js'), throwErr)
 
-// glob(Array.isArray(argv.c) ? argv.c.map(getComponentPaths) : getComponentPaths(argv.c))
-//   .then((componentPaths) => {
-//     fs.ensureFile(path.resolve(argv.d, 'global-definitions.js'), throwErr)
-//
-//     componentPaths.map(componentPath => {
-//       const componentPathName = getPathName(componentPath)
-//
-//       fs.readFile(componentPath, 'utf8', (err, code) => {
-//         if (err) throw err
-//
-//         fs.ensureFile(path.resolve(argv.d, componentPathName, 'definitions.js'), throwErr)
-//
-//         let props
-//         try {
-//           props = reactDocs.parse(code).props
-//         } catch(e) {
-//           console.log(`Could not parse component ${componentPathName}`)
-//         }
-//
-//         const types = Object.keys(props).reduce((prev, prop) => {
-//           const {
-//             type: {name},
-//             required
-//           } = props[prop]
-//
-//           prev[prop] = {props: [name, required]}
-//           return prev
-//         }, {});
-//
-//         const typesExport = (
-//           `export default ${inspect(types, false, null)}`
-//         )
-//
-//         fs.outputFile(path.resolve(argv.d, componentPathName, 'types.js'), typesExport, throwErr)
-//
-//         if (argv.m) {
-//           const component = `
-//             import React from 'react'
-//             import Reactionary, {specify} from '@adjohnston/reactionary'
-//             import c from '${path.resolve(componentPath)}'
-//             import gD from '../global-definitions'
-//             import t from './types'
-//             import d from './definitions'
-//             const s = specify(gD || {}, t || {}, d || {})
-//             export default Reactionary(s)(c)
-//           `
-//
-//           fs.outputFile(path.resolve(argv.d, componentPathName, 'component.js'), component, throwErr)
-//         }
-//       })
-//     })
-//
-//     if (argv.m) {
-//       const mapping = new Promise((res, rej) => {
-//         res(componentPaths.reduce((prev, componentPath) => {
-//           const componentPathName = getPathName(componentPath)
-//           const componentName = getComponentName(componentPathName)
-//
-//           return (
-//             prev += `import ${componentName} from '${path.resolve(argv.d, componentPathName, 'component')}';
-//             export {${componentName}};\n`
-//           )
-//         }, ''))
-//       }).then((map) => {
-//         fs.outputFile(path.resolve(argv.d, 'components.js'), map, throwErr)
-//       })
-//     }
-//   })
+      componentPaths.map(componentPath => {
+        const componentPathName = getPathName(componentPath)
+
+        fs.readFile(componentPath, 'utf8', (err, code) => {
+          if (err) throw err
+
+          fs.ensureFile(path.resolve(argv.d, componentPathName, 'definitions.js'), throwErr)
+
+          let props
+          try {
+            props = reactDocs.parse(code).props
+          } catch(e) {
+            console.log(`Could not parse component ${componentPathName}`)
+          }
+
+          const types = Object.keys(props).reduce((prev, prop) => {
+            const {
+              type: {name},
+              required
+            } = props[prop]
+
+            prev[prop] = {props: [name, required]}
+            return prev
+          }, {});
+
+          const typesExport = (
+            `export default ${inspect(types, false, null)}`
+          )
+
+          fs.outputFile(path.resolve(argv.d, componentPathName, 'types.js'), typesExport, throwErr)
+
+          if (argv.m) {
+            const component = `
+              import React from 'react'
+              import Reactionary, {specify} from '@adjohnston/reactionary'
+              import c from '${path.resolve(componentPath)}'
+              import gD from '../global-definitions'
+              import t from './types'
+              import d from './definitions'
+              const s = specify(gD || {}, t || {}, d || {})
+              export default Reactionary(s)(c)
+            `
+
+            fs.outputFile(path.resolve(argv.d, componentPathName, 'component.js'), component, throwErr)
+          }
+        })
+      })
+
+      if (argv.m) {
+        const mapping = new Promise((res, rej) => {
+          res(componentPaths.reduce((prev, componentPath) => {
+            const componentPathName = getPathName(componentPath)
+            const componentName = getComponentName(componentPathName)
+
+            return (
+              prev += `import ${componentName} from '${path.resolve(argv.d, componentPathName, 'component')}';
+              export {${componentName}};\n`
+            )
+          }, ''))
+        }).then((map) => {
+          fs.outputFile(path.resolve(argv.d, 'components.js'), map, throwErr)
+        })
+      }
+    })
+})
