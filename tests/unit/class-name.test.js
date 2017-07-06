1dbfe88fa4e3cdd112a3ab66c29fd61fa60a1110
Adam Johnston <adam.johnston3679@gmail.com>
2017-07-06 19:05:05 +0100

    className should now return stncl instead of stencil

--- tests/unit/class-name.test.js
+++ tests/unit/class-name.test.js
@@ -1,15 +1,15 @@
 import className from 'helpers/class-name'
 
 describe('#className', () => {
-  test(`given 'title' as an element param expect 'stencil__title'`, () => {
-    expect(className('title')).toBe('stencil__title')
+  test(`given 'title' as an element param expect 'stncl__title'`, () => {
+    expect(className('title')).toBe('stncl__title')
   })
 
-  test(`given 'large' as a modifier param expect 'stencil--large'`, () => {
-    expect(className(null, 'large')).toBe('stencil--large')
+  test(`given 'large' as a modifier param expect 'stncl--large'`, () => {
+    expect(className(null, 'large')).toBe('stncl--large')
   })
 
-  test(`given 'title' as an element param & 'large' as a modifier param expect 'stencil__title--large'`, () => {
-    expect(className('title', 'large')).toBe('stencil__title--large')
+  test(`given 'title' as an element param & 'large' as a modifier param expect 'stncl__title--large'`, () => {
+    expect(className('title', 'large')).toBe('stncl__title--large')
   })
 })
