fef6fdfe208fe304035416f14771bf9111056cbe
Adam Johnston <adam.johnston3679@gmail.com>
2017-07-06 13:59:37 +0100

    export as a named helper to be exported from stencil & and a default export so as tests dont need to be changed

--- src/helpers/merge.js
+++ src/helpers/merge.js
@@ -1,8 +1,8 @@
 import assign from 'assign-deep'
 
 //    merge : -> object : object -> object
-const merge = ({props, swatches} = {}, component) => {
+export const merge = ({props, swatches} = {}, component) => {
   return assign({}, {props, swatches}, component)
 }
 
-export { merge }
+export default merge
