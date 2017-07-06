3c560835b8ac95c7c68737d489d3b8c6a48d2742
Adam Johnston <adam.johnston3679@gmail.com>
2017-07-06 19:16:43 +0100

    class name helper was not updated to reflect changes made elsewhere

--- src/helpers/class-name.js
+++ src/helpers/class-name.js
@@ -1,6 +1,6 @@
 //    className : string -> string -> string
 const className = (element, modifier) => {
-  return `stencil${element ? `__${element}` : ''}${modifier ? `--${modifier}` : ''}`
+  return `stncl${element ? `__${element}` : ''}${modifier ? `--${modifier}` : ''}`
 }
 
 export default className
