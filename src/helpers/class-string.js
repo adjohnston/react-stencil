e064f5544603d61d2101a953d007c5a78ed6d51c
adjohnston <adam.johnston3679@gmail.com>
2018-06-25 15:07:13 +0100

    clean out old code

--- src/helpers/class-string.js
+++
@@ -1,8 +0,0 @@
-//    classString : string -> string
-const classString = (string) => {
-  if (typeof string !== 'string') return null
-
-  return `stncl${string}`
-}
-
-export default classString
--- src/stencil.js
+++ src/stencil.js
@@ -1,68 +1,16 @@
 import React, { Component } from 'react'
-import Documenter from 'src/components/Documenter'
 
-//    stencil : object -> component -> component
-const stencil = (specs = {}) => {
+//    withStencil : object -> component -> component
+const withStencil = (specs = {}) => {
   return WrappedComponent => {
-    return class Stencil extends Component {
-      constructor (props) {
-        super(props)
-
-        this.state = {
-          currentSwatch: '',
-          currentExample: 'default'
-        }
-
-        this.extendedExamples = Object.assign(specs.examples || {}, {
-          default: {...this.props}
-        })
-
-        this.setExample = this.setExample.bind(this)
-        this.setProp = this.setProp.bind(this)
-        this.setSwatch = this.setSwatch.bind(this)
-      }
-
-      setExample (example) {
-        this.setState({
-          currentExample: example,
-          ...this.extendedExamples[example]
-        })
-      }
-
-      setProp (prop, value) {
-        this.setState({[prop]: value})
-      }
-
-      setSwatch (swatch) {
-        this.setState({
-          currentSwatch: swatch
-        })
-      }
-
+    return class WithStencil extends Component {
       render () {
-        const {
-          currentSwatch,
-          currentExample,
-          ...componentState
-        } = this.state
-
-        const extendedProps = Object.assign({}, this.props, componentState)
         return (
-          <Documenter
-            setExample={this.setExample}
-            setProp={this.setProp}
-            setSwatch={this.setSwatch}
-            currentSwatch={currentSwatch}
-            currentExample={currentExample}
-            componentProps={extendedProps}
-            {...specs}>
-            <WrappedComponent {...extendedProps} />
-          </Documenter>
+          <WrappedComponent />
         )
       }
     }
   }
 }
 
-export { default as merge } from 'src/helpers/merge'
-export default stencil
+export withStencil
