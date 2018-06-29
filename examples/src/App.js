3457b6b690fab30c0db528c789858d01f4d2a949
adjohnston <adam.johnston3679@gmail.com>
2018-06-29 16:29:15 +0100

    examples

---
+++ examples/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component, Fragment } from 'react'
+import { withStencil, Name } from 'react-stencil'
+import { Switch } from '@learnerbly/web-components'
+
+const Template = ({ Component }) =>
+  <Fragment>
+    <Name>
+      { (name) => <h6 style={{ background: 'pink' }}>{name}</h6> }
+    </Name>
+
+    <Component />
+  </Fragment>
+
+const DocumentedMessage = withStencil({
+  // Template,
+
+  name: 'Switch',
+
+  description: 'This is an example of a switch component.',
+
+  props: {
+    value: true,
+    onChange: () => {},
+    label: 'I am on',
+    altLabel: 'I am off'
+  }
+})(Switch)
+
+class App extends Component {
+  render() {
+    return (
+      <DocumentedMessage />
+    )
+  }
+}
+
+export default App
